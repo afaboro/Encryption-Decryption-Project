3a94b4182b134257fa4376d6a7a7f14c21eba9fa
Anu <anufab04@gmail.com>
2019-08-05 16:29:01 -0400

    Completed Stage 4/6 in Encryption-Decryption Project Modified DecryptionSupport class to support command line arguments

--- Encryption-Decryption (Java)/task/src/encryptdecrypt/DecryptionSupport.java
+++ Encryption-Decryption (Java)/task/src/encryptdecrypt/DecryptionSupport.java
@@ -61,15 +61,141 @@ public class DecryptionSupport {
     }
 
 
-    public static void main(String[] args){
-        DecryptionSupport d = new DecryptionSupport();
+    /**
+     * This method incorporates command line arguments with 6 arguments
+     * Prints the encrypted/decrypted message
+     * @param args arguments from the command line
+     */
+    public void sixArgs(String[] args){
+        String mode = "";
+        String data = "";
+        int key = 0;
+
+        for(int i = 0; i < args.length; i++){
+            if(i+1 < args.length) {
+                if (i % 2 == 0) {
+                    switch (args[i]) {
+                        case "-mode":
+                            mode = args[i + 1];
+                            break;
+                        case "-key":
+                            key = Integer.parseInt(args[i + 1]);
+                            break;
+                        case "-data":
+                            data = args[i + 1];
+                            break;
+                    }
+                }
+            }
+        }
+        System.out.println(choose(mode, data, key));
+    }
+
+    /**
+     * This method incorporates command line arguments with 5 arguments
+     * Prints the encrypted/decrypted message
+     * @param args arguments from the command line
+     */
+    public void fiveArgs(String[] args){
+        String mode = "";
+        String data = "";
+        int key = 0;
+
+        for(int i = 0; i < args.length; i++){
+            switch(args[i]){
+                case "-mode":
+                    if(args[i+1].startsWith("-") || (i+1 == args.length)){
+                        mode = "enc";
+                    }
+                    break;
+                case "-key":
+                    key = Integer.parseInt(args[i+1]);
+                    break;
+                case "-data":
+                    data = args[i+1];
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println(choose(mode, data, key));
+    }
+
+    /**
+     * This method incorporates command line arguments with 4 arguments
+     * Prints the encrypted/decrypted message
+     * @param args arguments from the command line
+     */
+    public void fourArgs(String[] args){
+        String mode = "";
+        String data = "";
+        int key = 0;
         Scanner s = new Scanner(System.in);
 
-        String opt = s.nextLine();
-        String in = s.nextLine();
-        int key = s.nextInt();
+        for(int i = 0; i < args.length; i++){
+            switch(args[i]){
+                case "-mode":
+                    mode = args[i+1];
+                    break;
+                case "-data":
+                    data = s.nextLine();
+                    key = s.nextInt();
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println(choose(mode, data, key));
+    }
 
-        String o = d.choose(opt, in, key);
-        System.out.println(o);
+    /**
+     * This method incorporates command line arguments with 3 arguments
+     * Prints the encrypted/decrypted message
+     * @param args arguments from the command line
+     */
+    public void threeArgs(String[] args){
+        String mode = "";
+        String data = "";
+        int key = 0;
+
+        Scanner s = new Scanner(System.in);
+
+        for(int i = 0; i < args.length; i++){
+            switch(args[i]){
+                case "-mode":
+                    mode = "enc";
+                    break;
+                case "data":
+                    data = s.nextLine();
+                    key = s.nextInt();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+
+    public static void main(String[] args){
+        DecryptionSupport d = new DecryptionSupport();
+
+        if(args.length == 6){
+            d.sixArgs(args);
+        } else if(args.length == 5){
+            d.fiveArgs(args);
+        } else if(args.length == 4){
+            d.fourArgs(args);
+        } else if(args.length == 3){
+            d.threeArgs(args);
+        } else {
+            Scanner s = new Scanner(System.in);
+
+            String opt = s.nextLine();
+            String in = s.nextLine();
+            int key = s.nextInt();
+
+            String o = d.choose(opt, in, key);
+            System.out.println(o);
+        }
     }
 }
